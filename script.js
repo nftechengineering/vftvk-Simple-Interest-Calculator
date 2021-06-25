fa6a59db0f5ec962ca068f5b86b02de9cc6d2b91
nftechengineering <nftech.engineering@gmail.com>
2021-06-25 10:56:59 +0000

    fix error in displaying the result as as tring

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-var gains = interest+rate;
+var gains = interest+principal;
 
 document.getElementById("result1").innerText="If you deposit " + principal + ",";
 document.getElementById("result2").innerText="at an interest rate of " + rate +"%."; 

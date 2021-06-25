b65a15f918f16b895ac0a3ff85d0caf944ca0067
nftechengineering <nftech.engineering@gmail.com>
2021-06-25 11:39:02 +0000

    add console log for counter checking data

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function compute()
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
+var interest = principal + principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-let gains = principal + principal * years * rate /100;
 
 document.getElementById("result1").innerText="If you deposit " + principal + ",";
 document.getElementById("result2").innerText="at an interest rate of " + rate +"%."; 
 document.getElementById("result3").innerText="You will receive and amount of ";
-document.getElementById("gains").innerHTML=gains.value;
+document.getElementById("gains").innerHTML=interest;
 document.getElementById("result4").innerText="in the year " + year; 
 
 console.log(principal);
